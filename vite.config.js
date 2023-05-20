ad7dbcdeffd6d3adedfccb1d99b4f25dfe606694
yoriss67 <109190795+yoriss67@users.noreply.github.com>
2023-05-20 12:22:06 +0900

    Vite build /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Resume/', // Add this line
+  base: '/', // Add this line
 })
