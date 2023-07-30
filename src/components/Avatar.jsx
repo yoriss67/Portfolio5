923656cbb0a4ca30371caa47f0284ff7c0094ffe
yoriss67 <109190795+yoriss67@users.noreply.github.com>
2023-07-30 11:32:18 +0900

    Hide Avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -8,9 +8,9 @@ function Avatar() {
     <div className='avatar'>
    
       <h2>I’m learning React now!</h2>
-      <div className="canvas_pa ">
+      {/* <div className="canvas_pa ">
       <Spline className='spline' scene="https://prod.spline.design/o-qD5uBjwhHFWCAL/scene.splinecode" />
-      </div>
+      </div> */}
     </div>
   );
 }
